fe79469bab829f6831074705d672a3d13333236c
Rodrigo <ramos03rodrigo@gmail.com>
2021-11-07 00:21:51 +0000

    golden boy

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,12 +6,14 @@ const Projects = () => {
         <main className="">
           <section className="h-screen flex justify-center items-center">
 
-
-              <div className="grid grid-cols-1 gap-4 justify-items-center">
-                  <a href="http://vygado.pt"><img src="/vygado.png" className="rounded" /></a>
+              <div className="grid grid-cols-1 justify-items-center content-center">
+                <div className="relative">
+                  <div className="z-0 mix-blend-screen text-avogg -mb-2 text-3xl font-semibold"> Our golden boy</div> 
+                </div>
+                <div className="shadow-2xl p-10 bg-avogg rounded">
+                    <a href="http://vygado.pt"><img src="/vygado.png" className="rounded" /></a>
+                  </div>
               </div>
-
-
 
           </section>
         </main>
