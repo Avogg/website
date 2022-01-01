619dc099e416b3fab75ae544e13715e27657f7f9
Rodrigo <ramos03rodrigo@gmail.com>
2022-01-01 22:21:44 +0000

    feat--dowload-files

---
+++ pages/gallery/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const gallery = () => {
+    return (
+        <div>
+            <a className='bg-pink-300' href="miguelll.jpg" download>Download puto feio</a>
+        </div>
+    )
+}
+
+export default gallery
